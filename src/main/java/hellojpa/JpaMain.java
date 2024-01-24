17f09d9b4ff2f14e036b098aacaee6744eae1077
이아름 <areum6321@gmail.com>
2024-01-24 15:55:52 +0900

    상속관계 매핑 - 조인 전략 실행 클래스 변경

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,34 +19,24 @@ public class JpaMain {
 
         try {
 
-            //저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과 함께 사라지다");
+            movie.setPrice(10000);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            //연관관계의 주인에 값 설정 1
-            //member.changeTeam(team);
-            em.persist(member);
-
-            //team.getMembers().add(member);
-
-            team.addMember(member); //2
+            em.persist(movie);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for (Member m : members) {
-                System.out.println("m = " + m.getUsername());
-            }
+            //조회
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie =" + findMovie);
 
             tx.commit(); //DB에 쿼리 전송
         } catch (Exception e) {
-            tx.rollback(); //문제가 생겼을 때
+            tx.rollback();
         } finally {
             em.close();
         }
