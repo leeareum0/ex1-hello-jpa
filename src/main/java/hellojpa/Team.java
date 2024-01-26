0eff2b90c0e5bebbff6d4d5388079c42d6c4c8e4
이아름 <areum6321@gmail.com>
2024-01-26 22:35:44 +0900

    Team Class 불필요한 코드 주석처리

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -13,13 +13,13 @@ public class Team extends BaseEntity{
 
     private String name;
 
-    @OneToMany(mappedBy = "team") //읽기전용
-    private List<Member> members = new ArrayList<>();
+//    @OneToMany(mappedBy = "team") //읽기전용
+//    private List<Member> members = new ArrayList<>();
 
-    public void addMember(Member member) {
-        member.setTeam(this);
-        members.add(member);
-    }
+//    public void addMember(Member member) {
+//        member.setTeam(this);
+//        members.add(member);
+//    }
 
     public Long getId() {
         return id;
@@ -37,11 +37,11 @@ public class Team extends BaseEntity{
         this.name = name;
     }
 
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
+//    public List<Member> getMembers() {
+//        return members;
+//    }
+//
+//    public void setMembers(List<Member> members) {
+//        this.members = members;
+//    }
 }
